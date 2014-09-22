5e0fd540bf0ee5e74ff0f550f3981756adfc39c9
eisenbraun <krepta00@gmail.com>
2014-09-22 12:59:13 -0400

    Bug Fixes. Code Clean up. Image Optimization.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+/** DEBUGGING 
+var debugInput = document.querySelector("input");
+    function updateDebugState() {
+        document.body.classList.toggle('debug-on', debugInput.checked);
+    }
+    debugInput.addEventListener("click", updateDebugState);
+    updateDebugState();
+
+*/
